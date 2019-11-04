cfcf5f782e8842fc64475d757db1edfc5e23269b
umi <youhai1874@gmail.com>
2019-11-04 23:16:57 +0800

    使用element 进行layout布局

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # MyVue
+
 This is My Vue.Js Study Record Repository
 
 ## demo
+
 - demo1:主要是数据绑定和自定义方法的使用
+
+### Use Element UI
+
+- el-container
--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,34 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <p>{{message}}</p>
+    <el-container direction=‘vertical’>
+      <el-aside width="200px"><div>Aside</div></el-aside>
+      <el-container>
+        <el-header><div>Header</div></el-header>
+        <el-main><div>Main</div></el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    // HelloWorld
-  },
-  data(){
-    return{
-      message:"Umi You"
-    }
-  }
-}
+  name: "app"
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+
+#app > .el-container {
+  margin-bottom: 40px;
+  height: 100vh;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
