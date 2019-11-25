84d0c59a8262269ba8cc10eb9766c9824af10661
josephYou1874 <youhai1874@gmail.com>
2019-11-25 19:28:22 +0800

    Angular学习 hero

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-angular-app'`, () => {
+  it(`should have as title 'angular-tour-of-heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).toEqual('angular-tour-of-heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-angular-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heroes app is running!');
   });
 });
