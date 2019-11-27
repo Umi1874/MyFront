04d8236ae46344f4021f50527057624e27c6cfd6
josephYou1874 <youhai1874@gmail.com>
2019-11-27 12:25:36 +0800

    angularJs

---
+++ AngularJs/demo1.js
@@ -0,0 +1,4 @@
+var app1 = angular.module("app1", []);
+app1.controller("eventCtrl", function($scope) {
+    $scope.daybutton = true;
+});
--- my-vue-app/src/Bar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        Bar
-    </div>
-</template>
-<script>
-export default {
-}
-</script>
